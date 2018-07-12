af8620ac7a55770028109adaf6754fb1fa1437b6
Modsid <40784757+Modsid@users.noreply.github.com>
2018-07-13 03:05:01 +0530

    Update blockList.tsx

--- src/api/client/blockList.tsx
+++ src/api/client/blockList.tsx
@@ -119,11 +119,9 @@ export class BlockList extends React.Component<any, any> {
 <div className="jss466"><h1 className="jss313 jss319">Check Wallet Balance</h1></div>
 <div className="jss465"><div className="jss361"><div className="jss369 jss362 jss365">
 <label className="jss378 jss373 jss368 jss374 jss377" data-shrink="false" htmlFor="textField-search-field">Check address balance</label>
-<form>
 <div className="jss383 jss384 jss387">
-<input aria-invalid="false" aria-required="false" className="jss391" id="textField-search-field" onChange={(data) => this.handleBlockHash(data)} type="text" value=""/>
+<input aria-invalid="false" aria-required="false" className="jss391" id="textField-search-field" onChange={(data) => this.handleBlockHash(data)} onKeyPress={(event) => { if (event.key === "Enter") { this.searchBlock(event) } }} type="text"/>
 </div>
-</form>
 </div>
 <button  className="jss310 jss286 jss295 jss296 jss298 jss299 jss363" type="button" onClick={(event) => { this.searchBlock(event) } }> 
 <span className="jss287" >
