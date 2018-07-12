b91d2f08cc236c2bc77ddf098834522ace9573f1
Modsid <40784757+Modsid@users.noreply.github.com>
2018-07-12 22:29:46 +0530

    Update app.tsx

--- src/api/client/app.tsx
+++ src/api/client/app.tsx
@@ -187,11 +187,7 @@ export class App extends React.Component<{ rest: IRest }, any> {
 <input aria-invalid="false" aria-required="false" className="jss391" id="textField-search-field" onChange={(data) => this.handleBlockHash(data)} type="text" value=""/>
 </div>
 </div>
-<Button tabIndex="0" className="jss310 jss286 jss295 jss296 jss298 jss299 jss363" type="button"><span className="jss287"onClick={(event) => { this.searchBlock(event) } }} />
-<p className="jss313 jss322 jss364">SEARCH</p></span>
-<span className="jss337">
-</span>
-</Button>
+
 </div>
 </div>
 </div>
