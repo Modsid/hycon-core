9490ee988a7fb35ad86e2478b54ec29628c6a7e1
Modsid <40784757+Modsid@users.noreply.github.com>
2018-07-23 19:03:54 +0530

    Update blockList.tsx

--- src/api/client/blockList.tsx
+++ src/api/client/blockList.tsx
@@ -106,6 +106,9 @@ export class BlockList extends React.Component<any, any> {
             return < div ></div >
         }
             if (this.state.redirect) {
+                if (this.state.blockHash.length==43{
+                    return <Redirect to={`/tx/${this.state.blockHash}`} />
+}
             return <Redirect to={`/address/${this.state.blockHash}`} />
         }
  if (this.state.miner === undefined) {
@@ -121,7 +124,7 @@ export class BlockList extends React.Component<any, any> {
 <div className="jss465"><div className="jss361"><div className="jss369 jss362 jss365">
 <label className="jss378 jss373 jss368 jss374 jss377" data-shrink="true" htmlFor="textField-search-field"></label>
 <div className="jss383 jss384 jss387">
-<input aria-invalid="false" aria-required="false" className="jss391" id="textField-search-field" onChange={(data) => this.handleBlockHash(data)}  placeholder="Enter Address" onKeyPress={(event) => { if (event.key === "Enter") { this.searchBlock(event) } }} type="text"/>
+<input aria-invalid="false" aria-required="false" className="jss391" id="textField-search-field" onChange={(data) => this.handleBlockHash(data)}  placeholder="Enter Address/Transaction Hash" onKeyPress={(event) => { if (event.key === "Enter") { this.searchBlock(event) } }} type="text"/>
 </div>
 </div>
 <button  className="jss310 jss286 jss295 jss296 jss298 jss299 jss363" type="button" onClick={(event) => { this.searchBlock(event) } }> 
