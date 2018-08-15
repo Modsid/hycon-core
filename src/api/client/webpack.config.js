6eeffa0f49c9cc121c087967a55dfaaef57c7c46
Modsid <40784757+Modsid@users.noreply.github.com>
2018-08-16 02:00:30 +0530

    Update webpack.config.js

--- src/api/client/webpack.config.js
+++ src/api/client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
-      { test: /\.js$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {enforce: 'pre', test: /\.js$/, loader: 'source-map-loader'},
